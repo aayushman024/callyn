f6457d74872ef42cf49684ceb99e4fdec0264f9d
Aayushman Ranjan <aayushmanranjan24@gmail.com>
2026-06-22 12:41:44 +0530

    Bumped up the version code and version name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.mnivesh.callyn"
         minSdk = 27 // InCallService requires API 26 (Android 8.0)
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.6.0"
+        versionCode = 2
+        versionName = "1.6.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
